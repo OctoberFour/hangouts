dc28bd02087b30fcce4ecb25850ca7fdaa1630d9
Willian Justen <willianjustenqui@gmail.com>
2014-08-17 01:05:58 -0300

    Add compass task

--- performance/gulpfile.js
+++ performance/gulpfile.js
@@ -1,12 +1,27 @@
-var gulp = require('gulp')
-	concat = require('gulp-concat')
-	uglify = require('gulp-uglify');
+var env = require('minimist')(process.argv.slice(2)),
+	gulp = require('gulp'),
+	concat = require('gulp-concat'),
+	uglify = require('gulp-uglify'),
+	compass = require('gulp-compass'),
+	cssmin = require('gulp-cssmin'),
+	gulpif = require('gulp-if');
 
 gulp.task('js', function(){
 	return gulp.src('src/js/**/*.js')
 		.pipe(concat('main.js'))
-		.pipe(uglify())
+		.pipe(gulpif(env.p, uglify()))
 		.pipe(gulp.dest('build/js/'))
 });
 
-gulp.task('default', ['js']);
+gulp.task('css', function(){
+	return gulp.src('src/sass/main.scss')
+		.pipe(compass({
+			css: 'src/css/',
+			sass: 'src/sass/',
+			image: 'src/img'
+		}))
+		.pipe(gulpif(env.p, cssmin()))
+		.pipe(gulp.dest('build/css/'))
+});
+
+gulp.task('default', ['js', 'css']);
